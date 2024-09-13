c40cae36f744f44c719a44233fdaf2a3fc400623
Tuan Thai <titofood17122004@gmail.com>
2024-09-13 16:29:46 +0700

    new

--- website/views.py
+++ website/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
-from django.contrib import messages 
+from django.contrib import messages  # type: ignore
 from .forms import SignUpForm
 
 
 def home(request):
-    # Check logging 
+    return render(request, 'home.html', {})
+
+
+def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -21,13 +24,14 @@ def home(request):
             messages.success(request, "There was an error logging in, please try again ...")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
-
+        return render(request, 'login.html', {})
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out ... ")
     return redirect('home')
+
+
 
 
 def register_user(request):
